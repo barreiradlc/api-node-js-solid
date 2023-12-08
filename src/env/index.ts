f80178278efab02c7b6c280adf2d8dd19c929e2c
Augusto Barreira <barreira266@hotmail.com>
2023-12-08 10:01:53 -0300

    User creation

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,19 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-    PORT: z.coerce.number().default(3333)
-})
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
+});
 
-const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
-const { success } = _env
+const { success } = _env;
 
 if (success === false) {
-    console.error('Invalid env variables', _env.error.format())
+  console.error("Invalid env variables", _env.error.format());
 
-    throw new Error('Invalid env variables')
+  throw new Error("Invalid env variables");
 }
 
-export const env = _env.data
+export const env = _env.data;
